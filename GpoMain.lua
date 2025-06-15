bfbc436160cbad165d6a63aec947953021a48e60
Sanli Saeti <azukiairi777@gmail.com>
2025-06-15 16:47:34 +0700

    Update GpoMain.lua

--- GpoMain.lua
+++ GpoMain.lua
@@ -1,1 +1,8 @@
+Client = game.Players.LocalPlayer
+if getconnections then
+        for i,v in pairs(getconnections(Client.Idled)) do
+            v:Disable() 
+        end
+end
+wait()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2693ba4d4a0e7bee68d089f2a1ba4f37.lua"))()
