5404602a0bdefa9382c3a35f2b8cbe325f870c98
Sanli Saeti <azukiairi777@gmail.com>
2025-06-19 00:41:01 +0700

    Update GpoMain.lua

--- GpoMain.lua
+++ GpoMain.lua
@@ -1,8 +1,35 @@
-Client = game.Players.LocalPlayer
+local Client = game.Players.LocalPlayer
+
 if getconnections then
-        for i,v in pairs(getconnections(Client.Idled)) do
-            v:Disable() 
-        end
+    for i,v in pairs(getconnections(Client.Idled)) do
+        v:Disable() 
+    end
 end
-wait()
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2693ba4d4a0e7bee68d089f2a1ba4f37.lua"))()
+
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if not Client.Character then
+    Client.CharacterAdded:Wait()
+end
+
+repeat wait() until Client.Character and Client.Character:FindFirstChild("HumanoidRootPart")
+
+wait(2)
+
+local currentPlaceId = game.PlaceId
+
+if currentPlaceId == 11424731604 then
+    print("special place detected loading diff script")
+    pcall(function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ba4257029b2a29c97f99c9af8e471451.lua"))()
+    end)
+else
+    print("main gpo detected loading main script")
+    pcall(function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2693ba4d4a0e7bee68d089f2a1ba4f37.lua"))()
+    end)
+end
+
+print("loaded")
